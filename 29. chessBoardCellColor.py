c04c9311ddac3cdce6afbebdebd7c08f2a02f4ce
devarshh <107152704+devarshh@users.noreply.github.com>
2023-11-04 22:02:35 -0400

    Delete 29. chessBoardCellColor.py

--- 29. chessBoardCellColor.py
+++
@@ -1,7 +0,0 @@
-def solution(cell1, cell2):
-    cell1_total = ord(cell1[0]) + int(cell1[1])
-    cell2_total = ord(cell2[0]) + int(cell2[1])
-    if (cell1_total + cell2_total) % 2 == 0:
-        return True
-    else:
-        return False
