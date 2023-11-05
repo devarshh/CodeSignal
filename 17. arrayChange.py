2d94f735fe3774044ea316f84dc195c84523830d
devarshh <107152704+devarshh@users.noreply.github.com>
2023-11-04 21:58:12 -0400

    Delete 17. arrayChange.py

--- 17. arrayChange.py
+++
@@ -1,7 +0,0 @@
-def solution(inputArray):
-    total = 0
-    for i in range(1, len(inputArray)):
-        if inputArray[i] <= inputArray[i-1]:
-            total += inputArray[i-1] - inputArray[i] + 1
-            inputArray[i] = inputArray[i-1] + 1
-    return total
