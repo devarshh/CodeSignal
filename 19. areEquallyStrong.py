b5c27c5bf007d706c698f712a3b3f27dbb6562d9
devarshh <107152704+devarshh@users.noreply.github.com>
2023-11-04 21:58:30 -0400

    Delete 19. areEquallyStrong.py

--- 19. areEquallyStrong.py
+++
@@ -1,5 +0,0 @@
-def solution(yourLeft, yourRight, friendsLeft, friendsRight):
-    if max(yourLeft, yourRight) == max(friendsRight, friendsLeft) and min(yourLeft, yourRight) == min(friendsLeft, friendsRight):
-        return True
-    else:
-        return False
