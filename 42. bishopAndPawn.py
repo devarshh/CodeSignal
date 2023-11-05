6729cefce666f297415f6ab259e8fcf1e4450858
devarshh <107152704+devarshh@users.noreply.github.com>
2023-11-04 22:06:13 -0400

    Delete 42. bishopAndPawn.py

--- 42. bishopAndPawn.py
+++
@@ -1,7 +0,0 @@
-def solution(bishop, pawn):
-    if ord(bishop[0]) == ord(pawn[0]):
-        return False
-    else:
-        bishop_total = ord(bishop[0]) + int(bishop[1])
-        pawn_total = ord(pawn[0]) + int(pawn[1])
-        return (bishop_total + pawn_total) % 2 == 0
