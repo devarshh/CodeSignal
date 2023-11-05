32a7e2a46ef34bac97f00b3a5161e765d2043576
devarshh <107152704+devarshh@users.noreply.github.com>
2023-11-04 22:07:05 -0400

    Delete 44. findEmailDomain.py

--- 44. findEmailDomain.py
+++
@@ -1,7 +0,0 @@
-def solution(address):
-    split_add = address.split("@")
-    domain_part = [i for i in split_add]
-    if len(split_add) == 2:
-        return domain_part[1]
-    if len(split_add) == 3:
-        return domain_part[2]
