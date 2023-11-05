4ea323737f078a51eff5d0f273fa222ab80e756e
devarshh <107152704+devarshh@users.noreply.github.com>
2023-11-04 22:03:34 -0400

    Delete 32. absoluteValuesSumMinimization.py

--- 32. absoluteValuesSumMinimization.py
+++
@@ -1,8 +0,0 @@
-def solution(a):
-    sums = []
-    for i in range(len(a)):
-        summ = 0
-        for j in range(len(a)):
-            summ += abs(a[i] - a[j])
-        sums.append(summ)
-    return a[sums.index(min(sums))]
