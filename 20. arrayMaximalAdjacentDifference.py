b45d959c810ae6b79746d824656fc3772fb85fab
devarshh <107152704+devarshh@users.noreply.github.com>
2023-11-04 22:00:04 -0400

    Delete 20. arrayMaximalAdjacentDifference.py

--- 20. arrayMaximalAdjacentDifference.py
+++
@@ -1,7 +0,0 @@
-def solution(inputArray):
-    for i in range(len(inputArray)):
-        if i == 0:
-            cnt = 0
-        elif abs(inputArray[i] - inputArray[i-1]) >= cnt:
-            cnt = abs(inputArray[i] - inputArray[i-1])
-    return cnt
