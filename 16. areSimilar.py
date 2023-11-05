7630cd1735261dd0a88dbbc030b7ed04817b7127
devarshh <107152704+devarshh@users.noreply.github.com>
2023-11-04 21:56:18 -0400

    Delete 16. areSimilar.py

--- 16. areSimilar.py
+++
@@ -1,9 +0,0 @@
-def solution(A, B):
-    r = []
-    for i in range(len(A)):
-        if A[i] != B[i]:
-            r.append([A[i], B[i]])
-
-    if len(r) == 0 or len(r) == 2 and r[0] == r[1][::-1]:
-        return True
-    return False
